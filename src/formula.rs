426c63833babd9cf1fbf573bb0d3b9134542c8c8
Kristiyan Yochev <kris.yochev@gmail.com>
2023-01-24 18:26:52 +0000

    Added fmt::Display implementations for Formula and FormulaNode

--- src/formula.rs
+++ src/formula.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 pub struct Formula {
     root: FormulaLink,
 }
@@ -11,4 +13,24 @@ enum FormulaNode {
     Not(FormulaLink),
     Implication(FormulaLink, FormulaLink),
     Equivalence(FormulaLink, FormulaLink),
+}
+
+impl fmt::Display for Formula {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.root)
+    }
+}
+
+impl fmt::Display for FormulaNode {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use FormulaNode::*;
+        match self {
+            Variable(name) => write!(f, "{}", name),
+            And(lhs, rhs) => write!(f, "({} & {})", lhs, rhs),
+            Or(lhs, rhs) => write!(f, "({} | {})", lhs, rhs),
+            Not(formula) => write!(f, "!({})", formula),
+            Implication(lhs, rhs) => write!(f, "({} => {})", lhs, rhs),
+            Equivalence(lhs, rhs) => write!(f, "({} <=> {})", lhs, rhs),
+        }
+    }
 }
