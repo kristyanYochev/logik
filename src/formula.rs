d6a1f1828bc62f8e7d1cf75d2ac4059f693945c0
Kristiyan Yochev <kris.yochev@gmail.com>
2023-01-24 18:33:04 +0000

    Added constructor methods for formulae

--- src/formula.rs
+++ src/formula.rs
@@ -15,6 +15,38 @@ enum FormulaNode {
     Equivalence(FormulaLink, FormulaLink),
 }
 
+impl Formula {
+    fn from_node(node: FormulaNode) -> Self {
+        Self {
+            root: Box::new(node),
+        }
+    }
+
+    pub fn var(name: String) -> Self {
+        Self::from_node(FormulaNode::Variable(name))
+    }
+
+    pub fn and(lhs: Self, rhs: Self) -> Self {
+        Self::from_node(FormulaNode::And(lhs.root, rhs.root))
+    }
+
+    pub fn or(lhs: Self, rhs: Self) -> Self {
+        Self::from_node(FormulaNode::Or(lhs.root, rhs.root))
+    }
+
+    pub fn not(formula: Self) -> Self {
+        Self::from_node(FormulaNode::Not(formula.root))
+    }
+
+    pub fn implication(lhs: Self, rhs: Self) -> Self {
+        Self::from_node(FormulaNode::Implication(lhs.root, rhs.root))
+    }
+
+    pub fn equivalence(lhs: Self, rhs: Self) -> Self {
+        Self::from_node(FormulaNode::Equivalence(lhs.root, rhs.root))
+    }
+}
+
 impl fmt::Display for Formula {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", self.root)
